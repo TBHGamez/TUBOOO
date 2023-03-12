e4110944ac8ae7af78bf6fc2f30e75e864ee9229
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-03-12 20:45:47 +0700

    Update appp.py

--- appp.py
+++ appp.py
@@ -3,17 +3,17 @@ import streamlit as st
 import sklearn
 import plotly
 
-filename = 'model.pickle'
+# filename = 'model.pickle'
 
-pickle.dump(model, open(filename, "wb"))
-model = pickle.load(open(filename, "rb"))
+# pickle.dump(model, open(filename, "wb"))
+# model = pickle.load(open(filename, "rb"))
 # st.balloons()
 # y_pred = model.predict(x)
 # ii= mae(y, y_pred)
 
 st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
-x_new = st.number_input('blabla')
-y_new = model.predict(x_new)
+# x_new = st.number_input('blabla')
+# y_new = model.predict(x_new)
 
 if st.button('Giải'):
     st.success(f'dyasydyasd')
