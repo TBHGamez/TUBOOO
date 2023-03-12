7b8d96b99d7256a474fef47b7724927adda0ee6d
TBHGamez <127526165+TBHGamez@users.noreply.github.com>
2023-03-12 20:37:55 +0700

    Create appp.py

---
+++ appp.py
@@ -0,0 +1,18 @@
+import pickle
+import streamlit as st
+import sklearn
+
+filename = 'model.pickle'
+
+
+model = pickle.load(open(filename, "rb"))
+st.balloons()
+y_pred = model.predict(x)
+ii= mae(y, y_pred)
+
+st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
+#x_new = st.number_input('blabla')
+#y_new = model.predict(x_new)
+
+if st.button('Giải'):
+    st.success(f'{ii}')
